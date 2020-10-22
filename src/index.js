a4267465c641d0ef1880e11059d5404127bf878b
Vitali Siniak <43549823+nepp503@users.noreply.github.com>
2020-10-22 20:10:36 +0200

    init: added functionality for right brackets sequence check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let openBrackets =[];
+  let closedBrackets = [];
+  let sameBrackets = ['|', '7', '8'];
+  for(let i=0; i<bracketsConfig.length; i++){
+    openBrackets.push(bracketsConfig[i][0]);
+    closedBrackets.push(bracketsConfig[i][1]);
+  }
+  for(let letter of str){
+    if(openBrackets.includes(letter)){
+      if(result[result.length-1] === letter && sameBrackets.includes(letter)){
+        result.pop();
+      }else{
+        result.push(letter);
+      }
+    }else if(closedBrackets.includes(letter)){
+      let pair = openBrackets[closedBrackets.indexOf(letter)];
+      if(result[result.length-1] === pair){
+        result.pop();
+      }else{
+        result.push(letter);
+        break;
+      }
+    }
+  }
+  return (result.length === 0);
 }
